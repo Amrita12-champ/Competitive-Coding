5b59a6aa7bba2c4a5979ce987c08543e5b630e38
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-09-18 23:31:05 +0530

    Solved LeetCode Two Sum

---
+++ Leet-Practice/TwoSum.java
@@ -0,0 +1,20 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        TwoSum obj = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = obj.twoSum(nums, target);
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+    }
+}

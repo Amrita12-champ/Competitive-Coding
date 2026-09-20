704a2df3d8c5b46b03d5d0002b1743c5d02a4445
Amrita <286597828+Amrita12-champ@users.noreply.github.com>
2026-09-20 22:35:01 +0530

    Solved LeetCode Valid Parentheses

---
+++ Leet-Practice/ValidParentheses.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(') {
+                stack.push(')');
+            }
+            else if (c == '{') {
+                stack.push('}');
+            }
+            else if (c == '[') {
+                stack.push(']');
+            }
+            else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+
+        ValidParentheses obj = new ValidParentheses();
+
+        String s = "()[]{}";
+
+        System.out.println(obj.isValid(s));
+    }
+}
